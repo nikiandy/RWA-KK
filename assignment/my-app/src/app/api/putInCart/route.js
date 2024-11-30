a49bd6e39ff14655fa9f754c278b9fda9325f12a
nikiandy <B00152278@mytudublin.ie>
2024-11-30 19:15:19 +0000

    product page and add to cart functionality

--- assignment/my-app/src/app/api/putInCart/route.js
+++ assignment/my-app/src/app/api/putInCart/route.js
@@ -1,20 +1,46 @@
 export async function GET(req, res) {
   // Make a note we are on
+
   // the api. This goes to the console.
-  console.log("in the api page");
+
+  console.log("in the putInCart api page");
+
+  // get the values
+
+  // that were sent across to us.
+
+  const { searchParams } = new URL(req.url);
+
+  const pname = searchParams.get("pname");
+
+  console.log(pname);
+
   // =================================================
+
   const { MongoClient } = require("mongodb");
+
   const url =
     "mongodb+srv://nikiandy:Pass123@cluster0.zb6nt.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+
   const client = new MongoClient(url);
+
   const dbName = "app"; // database name
+
   await client.connect();
+
   console.log("Connected successfully to server");
+
   const db = client.db(dbName);
-  const collection = db.collection("products"); // collection name
-  const findResult = await collection.find({}).toArray();
-  console.log("Found documents =>", findResult);
+
+  const collection = db.collection("shopping_cart"); // collection name
+
+  var myobj = { pname: pname, username: "sample@test.com" };
+
+  const insertResult = await collection.insertOne(myobj);
+
   //==========================================================
+
   // at the end of the process we need to send something back.
-  return Response.json(findResult);
+
+  return Response.json({ data: "" + "inserted" + "" });
 }
